9faf4c4b62f27997ce869c0b7ef29615f4f286be
Crowdbotics <team@crowdbotics.com>
2020-11-29 06:52:14 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps179596', human_name: 'Maps', access_route: 'Maps179596', icon: 'map'},
 { name: 'Additem179595', human_name: 'AddItem', access_route: 'Additem179595'},
 { name: 'Maps179591', human_name: 'Maps', access_route: 'Maps179591', icon: 'map'},
 { name: 'UserProfile179587', human_name: 'User Profile', access_route: 'UserProfile179587'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps179596Navigator from '../features/Maps179596/navigator';
 import Additem179595Navigator from '../features/Additem179595/navigator';
 import Maps179591Navigator from '../features/Maps179591/navigator';
 import UserProfile179587Navigator from '../features/UserProfile179587/navigator';
@@ -16,6 +17,7 @@ import UserProfile179587Navigator from '../features/UserProfile179587/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Maps179596: { screen: Maps179596Navigator },
 Additem179595: { screen: Additem179595Navigator },
 Maps179591: { screen: Maps179591Navigator },
 UserProfile179587: { screen: UserProfile179587Navigator },
